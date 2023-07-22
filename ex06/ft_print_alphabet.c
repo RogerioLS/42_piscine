838497a33a470a5d3f8fe9700315dd2fc14b8432
RogLopes <roglopes@student.42sp.org.br>
2023-07-22 18:06:20 -0300

    :alien: .c print alfabeto

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roglopes <roglopes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 22:14:04 by roglopes          #+#    #+#             */
+/*   Updated: 2023/07/15 22:23:03 by roglopes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		ft_putchar(alphabet);
+		alphabet++;
+	}
+}
+
+/* int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
+ */
