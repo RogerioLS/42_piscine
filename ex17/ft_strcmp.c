a7a6dc4a710e6bd7b6c843244d416f17312f26f4
RogLopes <roglopes@student.42sp.org.br>
2023-07-22 18:31:38 -0300

    :alien: .c compara as string's

---
+++ ex17/ft_strcmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roglopes <roglopes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 16:22:26 by roglopes          #+#    #+#             */
+/*   Updated: 2023/07/16 16:38:24 by roglopes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/* #include <stdio.h>
+int	main(void)
+{
+	char	s1[] = "asdfghi";
+	char	s2[] = "asdfghj";
+
+	printf("%i", ft_strcmp(s1, s2));
+	return (0);
+} */
